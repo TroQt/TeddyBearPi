67be47bc56a08ca5b6babe832d6684a0ed647646
mexxme <danijel.brlekovic@ri.t-com.hr>
2020-07-23 09:39:43 +0200

    Initial commit

---
+++ src/TeddyBearPi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "TeddyBearPi.h"
+
+int init_board(void)
+{
+    printf("---------------Initialize TeddyBearPI board---------------\n");
+    return 0;
+}
+int my(void)
+{
+    printf("---------------RUN my---------------\n");
+    return 0;
+}
---
+++ src/TeddyBearPi.h
@@ -0,0 +1,13 @@
+/**
+* @brief Initialize TeddyBearPI board
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	extern int  init_board(void);
+	extern int  my(void);
+
+#ifdef __cplusplus
+}
+#endif
